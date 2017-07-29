82233abd58d7594b5952b28f1034fc36b5e5c844
tar-xzvff <jv120228m@gmail.com>
2017-07-29 22:35:08 +0900

    Django AdminにOSモデルを追加

--- tala/core/admin.py
+++ tala/core/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from core.models import Node
+from core.models import Node, OS
 
 
 class NodeAdmin(admin.ModelAdmin):
     pass
 
+
+class OSAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Node, NodeAdmin)
+admin.site.register(OS, OSAdmin)
