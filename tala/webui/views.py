5d4b0f912b12131ec3b67ea57e0db607f524aabc
tar-xzvff <jv120228m@gmail.com>
2017-07-29 19:06:05 +0900

    ビューを追加

--- tala/webui/views.py
+++ tala/webui/views.py
@@ -1,3 +1,14 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def node(request):
+    return render(request, 'nodes/index.html')
+
+
+def image(request):
+    return render(request, 'images/index.html')
