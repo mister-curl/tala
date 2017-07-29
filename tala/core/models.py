906885f15098466b30b157d43287a5db3ac0cba9
tar-xzvff <jv120228m@gmail.com>
2017-07-29 15:15:42 +0900

    Nodeモデルを追加

--- tala/core/models.py
+++ tala/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Node(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    ip_address = models.GenericIPAddressField(protocol='IPv4')
+    ipmi_ip_address = models.GenericIPAddressField(protocol='IPv4')
+    ipmi_user_name = models.CharField(max_length=200)
+    ipmi_password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
