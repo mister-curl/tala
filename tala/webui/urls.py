531e9de82589c038c5bc6c43401cf7110a3bccc6
tar-xzvff <jv120228m@gmail.com>
2017-07-29 19:05:43 +0900

    nodesとimagesのURLルーティング設定を追加

--- tala/webui/urls.py
+++ tala/webui/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^nodes/$', views.node, name='nodes'),
+    url(r'^images/$', views.image, name='images'),
 ]
