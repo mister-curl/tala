6b0f5e41739182b2b7080a67c6eb111869c41332
tar-xzvff <jv120228m@gmail.com>
2017-07-29 21:47:49 +0900

    ビューを追加

--- tala/webui/views.py
+++ tala/webui/views.py
@@ -1,5 +1,7 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from core.models import Node
 
 
 def index(request):
@@ -12,3 +14,13 @@ def node(request):
 
 def image(request):
     return render(request, 'images/index.html')
+
+
+class NodesView(ListView):
+    model = Node
+    template_name = 'nodes/index.html'
+
+
+class NodeView(DetailView):
+    model = Node
+    template_name = 'nodes/detail.html'
