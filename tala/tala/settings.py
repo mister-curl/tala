7e56ffa8c9714d0eb28d2573e71742b593407747
tar-xzvff <jv120228m@gmail.com>
2017-09-02 17:28:22 +0900

    INSTALLED_APPSにdjango_adminlteとchartjsを追加

--- tala/tala/settings.py
+++ tala/tala/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webui.apps.WebuiConfig'
+    'webui.apps.WebuiConfig',
+    'django_adminlte',
+    'django_adminlte_theme',
+    'chartjs',
 ]
 
 MIDDLEWARE = [
