84d38856945d4b49abc8ee292afdef3d13406756
tar-xzvff <jv120228m@gmail.com>
2017-09-02 21:41:20 +0900

    [WIP]サーバの使用率グラフを取得するエンドポイントを実装

--- tala/api/urls.py
+++ tala/api/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url, include
-from rest_framework import routers
+from rest_framework_nested import routers
 
 from api import views
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r'nodes', views.NodeViewSet)
+
+nodes_router = routers.NestedSimpleRouter(router, r'nodes', lookup='node')
+nodes_router.register(r'graphs', views.NodeGraphViewSet, base_name='node-graphs')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^', include(nodes_router.urls)),
 ]
--- tala/api/views.py
+++ tala/api/views.py
@@ -1,4 +1,7 @@
+import json
+
 from api.serializers import NodeSerializer
+from chartjs.views.lines import BaseLineChartView
 from core.models import Node
 from django.http import HttpResponse
 from rest_framework import viewsets, mixins, status
@@ -20,3 +23,29 @@ class NodeViewSet(viewsets.GenericViewSet,
             return HttpResponse({"error": "Not found."}, status=status.HTTP_404_NOT_FOUND)
         node.status = "READY"
         return HttpResponse("Status change completed.", status=status.HTTP_202_ACCEPTED)
+
+
+class NodeGraphViewSet(BaseLineChartView,
+                       viewsets.GenericViewSet,
+                       mixins.RetrieveModelMixin):
+    def get_labels(self):
+        """Return 7 labels for the x-axis."""
+        return ["January", "February", "March", "April", "May", "June", "July"]
+
+    def get_providers(self):
+        """Return names of datasets."""
+        return ["Central", "Eastside", "Westside"]
+
+    def get_data(self):
+        """Return 3 datasets to plot."""
+
+        return [[75, 44, 92, 11, 44, 95, 35],
+                [41, 92, 18, 3, 73, 87, 92],
+                [87, 21, 94, 3, 90, 13, 65]]
+
+    queryset = Node.objects.all()
+    serializer_class = NodeSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        print(kwargs)
+        return HttpResponse(json.dumps(super().get_context_data()))
