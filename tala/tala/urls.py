267574ab5944bb334b1e2a2cbca6ff62923c3c3a
tar-xzvff <jv120228m@gmail.com>
2017-09-02 18:35:32 +0900

    コンフリクト解消

--- tala/tala/urls.py
+++ tala/tala/urls.py
@@ -4,5 +4,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^tala/api/v1/', include('api.urls', namespace='api')),
-    url(r'^ui/', include('webui.urls')),
+    url(r'^ui/', include('webui.urls', namespace='webui')),
 ]
