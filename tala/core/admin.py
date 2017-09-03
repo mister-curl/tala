e1b43a8536ca747abda84de98a9fccaeb350ebb0
tar-xzvff <jv120228m@gmail.com>
2017-09-03 15:49:18 +0900

    カスタムユーザを認証に使うように変更

--- tala/core/admin.py
+++ tala/core/admin.py
@@ -1,6 +1,10 @@
+from django import forms
 from django.contrib import admin
 
 from core.models import Node, OS, VirtualMachine
+
+from core.models import User
+from django.contrib.auth.admin import UserAdmin
 
 
 class NodeAdmin(admin.ModelAdmin):
@@ -14,6 +18,40 @@ class OSAdmin(admin.ModelAdmin):
 class VirtualMachineAdmin(admin.ModelAdmin):
     pass
 
+
+class UserCreateForm(forms.ModelForm):
+    """ユーザ追加のためのフォーム。パスワードがハッシュで保存されないため追加しました。"""
+
+    class Meta:
+            model = User
+            fields = ('email', 'ssh_public_key')
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreateForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreateForm
+
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password', 'first_name', 'last_name', 'ssh_public_key')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password', 'first_name', 'last_name', 'ssh_public_key', 'is_superuser', 'is_staff', 'is_active')}
+         ),
+    )
+
+    filter_horizontal = ()
+
+
 admin.site.register(Node, NodeAdmin)
 admin.site.register(OS, OSAdmin)
 admin.site.register(VirtualMachine, VirtualMachineAdmin)
+admin.site.register(User, CustomUserAdmin)
--- tala/tala/settings.py
+++ tala/tala/settings.py
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+AUTH_USER_MODEL = 'core.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
