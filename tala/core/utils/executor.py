33a6ea58b7f6afbdabcb279566c442a1038ebf9f
tar-xzvff <jv120228m@gmail.com>
2017-09-11 12:15:25 +0900

    追加コマンドの実装

--- tala/core/utils/executor.py
+++ tala/core/utils/executor.py
@@ -12,36 +12,67 @@ def add(x, y):
 
 
 @app.task
-def bare_metal_create():
+def get_bare_metal_info():
     """
-    物理サーバに対してOSのインストールを行います
+    物理サーバに対して情報の取得を行います
+    BM 情報取得
     """
-    distribution = 'ubuntu1604_x86-64'
-    hostname = 'test1'
-    username = 'test'
-    root_password = 'test'
-    ipmi_ip_address = '192.168.125.5'
-    ipmi_username = 'admin'
-    ipmi_password = 'admin'
 
-    command = [SCRIPT_ROOT_DIR_PATH, 'bmcreate.sh', '-d', distribution, '-n', hostname, '-U', username, '-P', root_password, '-i', ipmi_ip_address, '-u', ipmi_username, '-p', ipmi_password]
+    host_id = 1
+
+    command = [SCRIPT_ROOT_DIR_PATH, 'bmgetinfo.sh', '-H', host_id, ]
     process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout = process.stdout.decode('utf-8')
     stderr = process.stderr.decode('utf-8')
 
 
 @app.task
-def get_bare_metal_info():
+def create_bare_metal():
     """
-    物理サーバに対して情報の取得を行います
+    物理サーバに対してOSのインストールを行います
+    ベアメタル作成
     """
+    host_id = 1
+    distribution = 'ubuntu1604_x86-64'
+    username = 'test'
 
-    host_id = 2
-    ipmi_ip_address = '192.168.125.5'
-    ipmi_username = 'admin'
-    ipmi_password = 'admin'
+    command = [SCRIPT_ROOT_DIR_PATH, 'bmcreate.sh', '-H', host_id, '-d', distribution, '-U', username]
+    process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout = process.stdout.decode('utf-8')
+    stderr = process.stderr.decode('utf-8')
 
-    command = [SCRIPT_ROOT_DIR_PATH, 'bmgetinfo.sh', '-H', host_id, '-i', ipmi_ip_address, '-u', ipmi_username, '-p', ipmi_password]
+
+@app.task
+def create_kvm_hyper_visor():
+    """
+    ベアメタルサーバからKVMホストを作成します
+    BM→KVMホスト化
+    """
+    host_id = 1
+
+    command = [SCRIPT_ROOT_DIR_PATH, 'kvmcreate.sh', '-H', host_id]
+    process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout = process.stdout.decode('utf-8')
+    stderr = process.stderr.decode('utf-8')
+
+
+@app.task
+def create_virtual_machine():
+    """
+    KVMホスト上にVMを作成します。
+    VM作成
+    """
+    host_id = 1
+    node_name = ''
+    allocate_cpu = 2
+    allocate_memory_size = 1024
+    allocate_disk_size = 10
+    vm_os = 'ubuntu1604_x86-64'
+    vm_password = 'test'
+
+    command = [SCRIPT_ROOT_DIR_PATH, 'vmcreate.sh', '-H', host_id, '-n', node_name, '-c', allocate_cpu,
+               '-m', allocate_memory_size, '-d', allocate_disk_size, '-o', vm_os, '-p', vm_password]
+
     process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout = process.stdout.decode('utf-8')
     stderr = process.stderr.decode('utf-8')
