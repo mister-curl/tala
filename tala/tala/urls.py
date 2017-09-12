a55bb8a3e516a0ae405f2916b1f64f9e941f4835
tar-xzvff <jv120228m@gmail.com>
2017-09-12 11:49:45 +0900

    /にアクセスしたさいに/ui/にリダイレクトするように変更

--- tala/tala/urls.py
+++ tala/tala/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', lambda x: HttpResponseRedirect('/ui/')),
     url(r'^tala/api/v1/', include('api.urls', namespace='api')),
     url(r'^ui/', include('webui.urls', namespace='webui')),
 ]
