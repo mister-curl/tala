08b0ede27e669032a49a08e9d8fb926c5da3674c
tar-xzvff <jv120228m@gmail.com>
2017-09-14 20:59:27 +0900

    コンフリクト解消

--- tala/webui/urls.py
+++ tala/webui/urls.py
@@ -12,12 +12,20 @@ urlpatterns = [
     url(r'^nodes/(?P<pk>[0-9]+)/delete/$', views.NodeDelete.as_view(), name='node-delete'),
     url(r'^nodes/(?P<pk>[0-9]+)/install/$', views.NodeOsInstall.as_view(), name='node-install'),
     url(r'^nodes/(?P<pk>[0-9]+)/kvm/$', views.NodeKvmCreate.as_view(), name='node-kvm'),
+
+    url(r'^nodes/(?P<pk>[0-9]+)/on/$', views.NodeKvmCreate.as_view(), name='node-power-on'),
+    url(r'^nodes/(?P<pk>[0-9]+)/off/$', views.NodeKvmCreate.as_view(), name='node-power-off'),
+    url(r'^nodes/(?P<pk>[0-9]+)/restart/$', views.NodeKvmCreate.as_view(), name='node-power-restart'),
+
     url(r'^nodes/(?P<pk>[0-9]+)/$', views.NodeView.as_view(), name='node'),
 
     url(r'^virtualmachines/$', views.VirtualMachinesView.as_view(), name='virtualmachines'),
     url(r'^virtualmachines/create$', views.VirtualMachineCreateView.as_view(), name='virtualmachine-create'),
     url(r'^virtualmachines/(?P<pk>[0-9]+)/delete/$', views.VirtualMachineDelete.as_view(), name='virtualmachine-delete'),
     url(r'^virtualmachines/(?P<pk>[0-9]+)/$', views.VirtualMachineView.as_view(), name='virtualmachine'),
+    url(r'^virtualmachines/(?P<pk>[0-9]+)/on/$', views.VirtualMachineView.as_view(), name='vm-power-on'),
+    url(r'^virtualmachines/(?P<pk>[0-9]+)/off/$', views.VirtualMachineView.as_view(), name='vm-power-off'),
+    url(r'^virtualmachines/(?P<pk>[0-9]+)/restart/$', views.VirtualMachineView.as_view(), name='vm-power-restart'),
 
     url(r'^users/$', views.UsersView.as_view(), name='users'),
     url(r'^users/create/$', views.UserCreate.as_view(), name='user-create'),
