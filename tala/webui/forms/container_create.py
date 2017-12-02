71af179d185d222b093038c6ad83b35bc50e0879
tar-xzvff <jv120228m@gmail.com>
2017-12-02 12:13:38 +0900

    コンテナを作る処理を追加

--- tala/webui/forms/container_create.py
+++ tala/webui/forms/container_create.py
@@ -1,11 +1,7 @@
-from django import forms
-
-from core.models import VirtualMachine
-
-from core.utils.executor import create_virtual_machine
-from django.forms import Select
-
 from core.models import Container
+from core.utils.executor import create_container
+from django import forms
+from django.forms import Select
 
 
 class ContainerCreateForm(forms.ModelForm):
@@ -15,13 +11,14 @@ class ContainerCreateForm(forms.ModelForm):
 
     OS_CHOICES = (
         ('ubuntu1604_x86-64', 'Ubuntu 16.04'),
+        ('ubuntu1404_x86-64', 'Ubuntu 14.04'),
     )
 
     os = forms.ChoiceField(widget=Select, choices=OS_CHOICES)
 
     def save(self, commit=True):
         container = super(ContainerCreateForm, self).save(commit=True)
-        #create_virtual_machine.delay(1, "test")
+        create_container.delay(container.id)
         if commit:
             pass
         return container
