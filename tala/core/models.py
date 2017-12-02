1c8c0f4efd53e0c7255d9e6e6703cc5b63bb6804
tar-xzvff <jv120228m@gmail.com>
2017-12-02 11:36:22 +0900

    コンテナのモデルを追加

--- tala/core/models.py
+++ tala/core/models.py
@@ -68,6 +68,46 @@ class VirtualMachine(models.Model):
     vnc_port = models.IntegerField("VNCポート番号", blank=True, null=True)
 
 
+class Container(models.Model):
+    #CPU_CHOICES = (
+    #    ('1', '1'),
+    #    ('2', '2'),
+    #    ('3', '3'),
+    #    ('4', '4'),
+    #)
+
+    # MB
+    #EMORY_CHOICES = (
+    #    ('512', '512MB'),
+    #    ('1024', '1GB'),
+    #    ('2048', '2GB'),
+    #    ('4096', '4GB'),
+    #)
+
+    # GB
+    #DISK_CHOICES = (
+    #    ('10', '10GB'),
+    #    ('20', '20GB'),
+    #    ('50', '50GB'),
+    #    ('100', '100GB'),
+    #)
+
+    name = models.CharField(max_length=200, blank=True)
+    description = models.CharField(max_length=200, blank=True)
+    hostname = models.CharField(max_length=200, blank=True)
+    ip_address = models.GenericIPAddressField(protocol='IPv4', null=True, blank=True)
+    mac_address = models.CharField(max_length=200, blank=True)
+    os = models.CharField(max_length=200, blank=True)
+    status = models.CharField(max_length=200, blank=True)
+    power = models.CharField(max_length=200, blank=True)
+    docker_host = models.ForeignKey(Node, related_name='virtual_machines')
+    password = models.CharField("管理ユーザパスワード", max_length=200, blank=True)
+    #allocate_cpu = models.CharField("割り当てCPUコア数", max_length=200, blank=True, choices=CPU_CHOICES)
+    #allocate_memory = models.CharField("割り当てメモリ容量", max_length=200, blank=True, choices=MEMORY_CHOICES)
+    #allocate_disk = models.CharField("割り当てディスク容量", max_length=200, blank=True, choices=DISK_CHOICES)
+    #vnc_port = models.IntegerField("VNCポート番号", blank=True, null=True)
+
+
 class OS(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
